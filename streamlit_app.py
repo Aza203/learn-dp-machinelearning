f5aee9122b019dd69de1f8b6d09968b72b1bcb95
Aza203 <115408410+Aza203@users.noreply.github.com>
2024-11-06 14:23:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
 if 'data' not in st.session_state:
   st.session_state['data'] = []
@@ -27,10 +28,13 @@ if page == "Input Data":
       
   if st.button('Simpan Data'):
     if driver_name and instance_name and money_amount:
+      current_time = datetime.now()
       st.session_state['data'].append({
         'Nama Driver' : driver_name,
         'Nama Instansi': instance_name,
-        'Jumlah Uang': money_amount
+        'Jumlah Uang': money_amount,
+        'Tanggal': current_time.strftime("%Y-%m-%d"),
+        'Waktu': current_time.strftime("%H:%M:%S")
       })
       st.success('Data Berhasil disimpan!')
     else:
@@ -40,6 +44,10 @@ elif page == 'List Data':
   st.title('List Data')
   if st.session_state['data']:
     df = pd.DataFrame(st.session_state['data'])
-    st.table(df)
+    df['Tanggal'] = pd.to_datetime(df['Tanggal']) 
+    grouped = df.groupby([df['Tanggal'].dt.year, df['Tanggal'].dt.month, df['Tanggal'].dt.day])
+    for (year, month,day), group in grouped:
+      st.write(f"Data untuk {year}-{month:02d}-{day:02d}")
+      st.table(group)
   else:
     st.write("Belum ada data yang disimpan")
