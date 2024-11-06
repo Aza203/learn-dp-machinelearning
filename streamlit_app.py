b0aab21f1554256e91f824aa6e24d28a1fdabd4c
Aza203 <115408410+Aza203@users.noreply.github.com>
2024-11-06 09:17:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Machine Learning Apps')
 
 st.write('this is app for learn machine learning')
+
+st.sidebar.title('Navigasi')
+page = st.sidebar.selectbox("Pilih Halaman",["input data", "List Data", "Grafik", "Forecasting"])
