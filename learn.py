d8de0953cfd58e3c96ee6fba702f1fb76f88a9d6
Aza203 <115408410+Aza203@users.noreply.github.com>
2024-11-10 13:22:09 +0700

    Update learn.py

--- learn.py
+++ learn.py
@@ -7,8 +7,10 @@ if 'angka' not in st.session_state:
 # Input nilai yang akan ditambahkan
 nilai_tambah = st.number_input('Masukkan nilai yang akan ditambahkan:', min_value=0, step=1)
 
-# Tambahkan nilai input ke nilai yang ada
-st.session_state.angka += nilai_tambah
+# Tombol submit
+if st.button('Submit'):
+    # Tambahkan nilai input ke nilai yang ada
+    st.session_state.angka += nilai_tambah
 
 # Tampilkan hasil
 st.write('Hasil penambahan:', st.session_state.angka)
